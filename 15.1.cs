cd234cc02035d3f3cf89c7b2a636025abe87ab44
ko5eyka <ryslanboy0111@gmail.com>
2025-11-07 00:16:32 +0300

    Add files via upload

---
+++ 15.1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._1
+{
+    interface IWorking
+    {
+        void Work();
+    }
+    interface IReloading
+    {
+        void Reload();
+    }
+    class Robot: IWorking, IReloading
+    {
+        private string name;
+        private int energy;
+            public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Energy
+        {
+            get { return energy; }
+            set
+            {
+                if (value >= 0 && value <= 100) { energy = value; }
+                else
+                {
+                    Console.WriteLine("Энергия должна быть от 0 до 100!");
+                }
+            }
+        }
+        public Robot(string name, int energy)
+        {
+            Name = name;
+            Energy = energy;
+        }
+        public void Work()
+        {
+            if (Energy >= 20)
+            {
+                Energy -= 20;
+                Console.WriteLine($"Робот работает...\nИмя: {Name}, энергия: {Energy}\n");
+            } else
+            {
+                Energy -= 0;
+                Console.WriteLine("Недостаточно энергии! Робот не может работать :(");
+                Console.WriteLine($"Робот: {Name}, энергия: {Energy}\n");
+            }
+        }
+        public void Reload()
+        {
+            Energy += 50;
+            if (Energy > 100)
+            {
+                Energy = 100;
+            }
+            Console.WriteLine($"Робот заряжается...\nИмя: {Name}, энергия: {Energy}\n");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot[] rob = { new Robot("Валера", 90) };
+            foreach (Robot r in rob)
+            {  
+                r.Work();
+                r.Work(); 
+                r.Reload();
+                r.Work();
+            }
+        }
+    }
+}
---
+++ 15.2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._2
+{
+    interface OnOff
+    {
+        void On();
+        void Off();
+    }
+    interface SettingTheLevel
+    {
+        void PrintLevel(int level);
+    }
+    class Lamp: OnOff, SettingTheLevel
+    {
+        private int level { get; set; } = 100;
+        public int Level
+        {
+            get {  return level; }
+            set
+            {
+                if (value >= 0 && value <= 100) { level = value; }
+                else
+                {
+                    Console.WriteLine("Уровень должен быть от 0 до 100!");
+                }
+            }
+        }
+        private int getLevel { get; set; } = 100;
+       
+        public void On()
+        {
+            Level = getLevel;
+            Console.WriteLine($"Лампа включена. Уровень: {Level}");
+        }
+        public void Off()
+        {
+            Level = 0;
+            Console.WriteLine($"Лампа выключена. Уровень: {Level}");
+        }
+        public void PrintLevel(int level)
+        {
+            getLevel = level;
+            Level = level;
+            Console.WriteLine($"Установлен уровень: {Level}");
+        }
+    }
+    class Fan: OnOff
+    {
+        public void On()
+        {
+            Console.WriteLine("Вентилятор включен.");
+        }
+        public void Off()
+        {
+            Console.WriteLine("Вентилятор выключен.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lamp[] lamp = { new Lamp() };
+            foreach (Lamp l  in lamp)
+            {
+               l.PrintLevel(30);
+                l.On();
+                l.Off();
+                l.On();
+                l.Off();
+                l.On();
+            }
+            Console.WriteLine();
+            Fan[] fan = { new Fan() };
+            foreach (Fan f  in fan)
+            {
+                f.On();
+                f.Off();
+            }
+        }
+    }
+}
